dcde9cfe70016ece7238e6f54f2b2f9baea3df93
Mitch Malinin <mitchmalinin@gmail.com>
2020-04-19 20:35:11 -0400

    drink orderes

--- socketio/users.js
+++ socketio/users.js
@@ -1,8 +1,9 @@
 const users = [];
+const drinkQue = [];
 
-const addUser = ({ id, name, room }) => {
-  name = name.trim().toLowerCase();
-  room = room.trim().toLowerCase();
+const addUser = ({ id, userName, roomName }) => {
+  name = userName.trim().toLowerCase();
+  room = roomName.trim().toLowerCase();
 
   const existingUser = users.find(
     (user) => user.room === room && user.name == name
@@ -20,7 +21,6 @@ const addUser = ({ id, name, room }) => {
 
 const removeUser = (id) => {
   const index = users.findIndex((user) => user.id === id);
-  console.log("index", index);
   if (index !== -1) {
     return users.splice(index, 1)[0];
   }
@@ -36,4 +36,38 @@ const getUsersInRoom = (room) => {
   });
 };
 
-module.exports = { addUser, removeUser, getUser, getUsersInRoom };
+const addDrinkOrder = ({ id, userName, roomName, drinkID }) => {
+  name = userName.trim().toLowerCase();
+  room = roomName.trim().toLowerCase();
+  const userOrder = { id, name, room, drinkID };
+  drinkQue.push(userOrder);
+  return userOrder;
+};
+
+const getAllDrinkOrders = (room) => {
+  // console.log("this is the room", room);
+  let filtered = drinkQue.filter((user) => {
+    return user.room === room;
+  });
+  return filtered;
+};
+
+const removeDrinksPerUser = (id) => {
+  for (var i = drinkQue.length - 1; i >= 0; i--) {
+    if (drinkQue[i].id == id) {
+      drinkQue.splice(i, 1);
+    }
+  }
+  console.log("this is the new drink que", drinkQue);
+  return drinkQue;
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+  addDrinkOrder,
+  getAllDrinkOrders,
+  removeDrinksPerUser,
+};
--- twilioserver.js
+++ twilioserver.js
@@ -3,8 +3,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
 const { chatToken, videoToken, voiceToken } = require("./tokens");
+const http = require("http");
 const cors = require("cors");
+const socketio = require("socket.io");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+  addDrinkOrder,
+  getAllDrinkOrders,
+  removeDrinksPerUser,
+} = require("./socketio/users");
+//setting up the server
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(cors());
 const PORT = process.env.PORT || 3001;
@@ -70,4 +84,95 @@ app.post("/voice/token", (req, res) => {
   sendTokenResponse(token, res);
 });
 
-app.listen(PORT, () => console.log(`Express server is running on ${PORT}`));
+//setting up a socket
+io.on("connection", (socket) => {
+  console.log("We have a new connection");
+  socket.on("join", ({ userName, roomName }, callback) => {
+    const { error, user } = addUser({ id: socket.id, userName, roomName });
+    console.log("this is the user", user);
+    if (error) return callback(error);
+
+    socket.broadcast.to(user.room).emit("message", {
+      user: "Chatbot",
+      text: `${user.name} joined ${room}`,
+    });
+
+    socket.join(user.room);
+
+    io.to(user.room).emit("roomData", {
+      users: getUsersInRoom(user.room),
+    });
+
+    io.to(user.room).emit("allDrinkOrders", {
+      userOrders: getAllDrinkOrders(user.room),
+    });
+
+    callback();
+  });
+
+  // socket.on("sendMessage", (message, callback) => {
+  //   const user = getUser(socket.id);
+  //   io.to(user.room).emit("message", { user: user.name, text: message });
+  //   callback();
+  // });
+
+  socket.on("drinkOrder", ({ userName, roomName, drinkID }) => {
+    console.log("drink order", userName, roomName, drinkID);
+    const newOrder = addDrinkOrder({
+      id: socket.id,
+      userName,
+      roomName,
+      drinkID,
+    });
+
+    socket.broadcast.to(newOrder.room).emit("newOrder", {
+      newOrder,
+    });
+
+    io.to(newOrder.room).emit("allDrinkOrders", {
+      userOrders: getAllDrinkOrders(newOrder.room),
+    });
+  });
+
+  socket.on("disconnectWhenLoggingOut", () => {
+    const user = removeUser(socket.id);
+    console.log("hey i am being disconected", user);
+    removeDrinksPerUser(socket.id);
+    if (user) {
+      console.log("user", user);
+      io.to(user.room).emit("message", {
+        user: "Chatbot",
+        text: `${user.name} disconnected`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+      io.to(user.room).emit("allDrinkOrders", {
+        userOrders: getAllDrinkOrders(user.room),
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    console.log("hey i am being disconected", user);
+    removeDrinksPerUser(socket.id);
+    if (user) {
+      console.log("user", user);
+      io.to(user.room).emit("message", {
+        user: "Chatbot",
+        text: `${user.name} disconnected`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+      io.to(user.room).emit("allDrinkOrders", {
+        userOrders: getAllDrinkOrders(user.room),
+      });
+    }
+  });
+});
+
+server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
